ad86d8a087c8925aa19c096b5c8a3c6603d73a0b
Otmakhov Dmitriy <otmakhov1990dima@mail.ru>
2020-10-23 16:03:37 +0700

    Очередное изменение проекта

---
+++ App/Data/Product/ProductRepository.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Data\Product;
+
+
+use App\Model\AbstractRepository;
+
+/**
+ * Class ProductRepository
+ * @package App\Data\Product
+ *
+ * @Model(App\Data\Product\ProductModel)
+ */
+class ProductRepository extends AbstractRepository
+{
+
+}
---
+++ App/Data/User/UserRepository.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Data\User;
+
+
+use App\Model\AbstractRepository;
+
+/**
+ * Class UserRepository
+ * @package App\Data\User
+ *
+ * @Model(App\Data\User\UserModel)
+ */
+class UserRepository extends AbstractRepository
+{
+
+}
